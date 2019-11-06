d885058a300fcc3c5c7c641b1b42ffa7d90b446c
FernandoDeOliveira <linhares.lfoliveira@gmail.com>
2019-11-05 23:37:33 -0300

    iluminacao ainda incompleta

---
+++ lighting.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <GL/freeglut.h>
+
+namespace lighting
+{
+#define PI 3.1415
+
+    int window, end = 0;
+
+    GLint WIDTH = 320;
+    GLint HEIGHT = 240;
+
+    GLint slice = 30;
+    GLint pile = 30;
+    GLint sphereRadius = 1.5;
+
+    GLfloat obs[3] = {0.0, 7.0, 0.0};
+    GLfloat eyes[3] = {0.0, 3.0, 0.0};
+
+    GLfloat  
+
+
+    
+} // namespace lighting
